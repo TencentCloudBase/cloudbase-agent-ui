c7cbb8a21782f0117a73b42c95abbd38891b3e14
ryan <ryanhhuang@tencent.com>
2025-03-27 16:49:32 +0800

    feat: 环境共享迁移完成

--- apps/miniprogram-agent-ui/miniprogram/pages/chatBot/chatBot.js
+++ apps/miniprogram-agent-ui/miniprogram/pages/chatBot/chatBot.js
@@ -4,17 +4,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    chatMode: "bot", // bot 表示使用agent，model 表示使用大模型
+    chatMode: "model", // bot 表示使用agent，model 表示使用大模型
     showBotAvatar: true, // 是否在对话框左侧显示头像
-    envShareConfig:{ // 不使用环境共享，请删除此配置或配置EnvShareConfig:null
-      // 资源方 AppID
-      resourceAppid: 'wx7ac1bfecc7bf5f4f',
-      // 资源方环境 ID
-      resourceEnv: 'chriscc-demo-7ghlpjf846d46d2d',
-    },
-    aiInstance: null, // todo 显示表明使用环境共享的ai能力
+    // envShareConfig: {
+    //   // 不使用环境共享，请删除此配置或配置EnvShareConfig:null
+    //   // 资源方 AppID
+    //   resourceAppid: "wx7ac1bfecc7bf5f4f",
+    //   // 资源方环境 ID
+    //   resourceEnv: "chriscc-demo-7ghlpjf846d46d2d",
+    // },
     agentConfig: {
-      botId: "bot-a186af8b", // agent id,
+      botId: "bot-9e014f20", // agent id,
       allowWebSearch: true, // 允许客户端选择启用联网搜索
       allowUploadFile: true, // 允许上传文件
       allowPullRefresh: true, // 允许下拉刷新
@@ -24,56 +24,47 @@ Page({
     modelConfig: {
       modelProvider: "deepseek", // 大模型服务厂商
       quickResponseModel: "deepseek-v3", // 快速响应模型 （混元 turbo, gpt4 turbo版，deepseek v3等）
-      logo: "https://qcloudimg.tencent-cloud.cn/raw/e242f8defd4cac557b650ad8d1c1a473.jpeg", // model 头像
+      logo: "", // model 头像
       welcomeMsg: "欢迎语", // model 欢迎语
     },
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  async onLoad(options) {
-    var instance = new wx.cloud.Cloud({
-      // 资源方 AppID
-      resourceAppid: 'wx7ac1bfecc7bf5f4f',
-      // 资源方环境 ID
-      resourceEnv: 'chriscc-demo-7ghlpjf846d46d2d',
-    })
-    await instance.init()
-    this.setData({aiInstance:instance.extend.AI})
-  },
+  onLoad(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() { },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() { },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() { },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() { },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() { },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() { },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() { },
+  onShareAppMessage() {},
 });
